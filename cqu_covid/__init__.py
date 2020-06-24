fa2de02d8ebb890dc4c45578d625f44dc6269d9c
sorphwer <sorphwer@163.com>
2020-06-24 12:44:45 +0800

    replace yaml config into config.py

--- cqu_covid/__init__.py
+++ cqu_covid/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
-import yaml
+import config
+# import yaml
 app = Flask(__name__, static_folder="templates")
 #init app config
-def read_yaml(yaml_file_path):
-    with open(yaml_file_path, 'rb') as f:
-        cf= yaml.safe_load(f.read()) 
-        #yaml.load(f.read())
-    return cf
-cf = read_yaml("_config.yaml")
-app.config.update(cf)
-
+# def read_yaml(yaml_file_path):
+#     with open(yaml_file_path, 'rb') as f:
+#         cf= yaml.safe_load(f.read()) 
+#         #yaml.load(f.read())
+#     return cf
+# cf = read_yaml("_config.yaml")
+# app.config.update(cf)
+app.config.from_object(config) 
 #import app modules
 import cqu_covid.views
